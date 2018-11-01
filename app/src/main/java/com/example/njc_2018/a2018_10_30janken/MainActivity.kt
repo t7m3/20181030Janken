1337188e1fb63648937b7ec9966e358dbe7f231a
NJC-T1 <toda7.mk3@gmail.com>
2018-11-01 12:45:26 +0900

    2018-11-01 １回目のコミット（共有プリファレンスによるデータの保存）

--- app/src/main/java/com/example/njc_2018/a2018_10_30janken/MainActivity.kt
+++ app/src/main/java/com/example/njc_2018/a2018_10_30janken/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.njc_2018.a2018_10_30janken
 
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.preference.PreferenceManager
+import android.support.v7.app.AppCompatActivity
 import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,6 +17,11 @@ class MainActivity : AppCompatActivity() {
         gu.setOnClickListener { onJankenButtonTapped(it) }
         choki.setOnClickListener { onJankenButtonTapped(it) }
         pa.setOnClickListener { onJankenButtonTapped(it) }
+
+        //共有プリファレンスのクリア
+        val pref = PreferenceManager.getDefaultSharedPreferences(this)
+        val editor = pref.edit()
+        editor.clear().apply()
     }
 
     fun onJankenButtonTapped(view: View?) {
